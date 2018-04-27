657da01d2e572fbe3b087da3621ed3ceead3b557
dicleyamanoglu <37039118+dicleyamanoglu@users.noreply.github.com>
2018-04-27 14:04:29 +0300

    Delete soru1m.py

--- soru1m.py
+++
@@ -1,7 +0,0 @@
-from odev import *
-gelir=int(input("Geliri giriniz:"))
-gider=int(input("Gideri giriniz:"))
-isleKar(gelir,gider)
-topci=int(input("Toplam ciroyu giriniz:"))
-calisan=int(input("Toplam Çalışan sayısını giriniz:"))
-adambasiC(topci,calisan)
