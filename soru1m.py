2634ad488150b607d242e51cd75bbe715c0cd26e
dicleyamanoglu <37039118+dicleyamanoglu@users.noreply.github.com>
2018-04-27 14:06:06 +0300

    Add files via upload

---
+++ soru1m.py
@@ -0,0 +1,7 @@
+from soru1 import *
+gelir=int(input("Geliri giriniz:"))
+gider=int(input("Gideri giriniz:"))
+isleKar(gelir,gider)
+topci=int(input("Toplam ciroyu giriniz:"))
+calisan=int(input("Toplam Çalışan sayısını giriniz:"))
+adambasiC(topci,calisan)
